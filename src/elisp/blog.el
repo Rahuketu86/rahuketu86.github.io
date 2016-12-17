17b21f0b35985e56b9c90651c66ee3622f654d29
Rahul Saraf <Rahul Saraf@Ipcos.local>
2016-12-18 02:54:29 +0530

    Added a navigation menu and fixed code for blog export.

--- src/elisp/blog.el
+++ src/elisp/blog.el
@@ -1,17 +1,40 @@
+(require 'org)
+
 (setq org-mode-websrc-directory  "C:/rahuketu/programming/static-site/rahuketu86.github.io/src" )
 (setq org-mode-publishing-directory  "C:/rahuketu/programming/static-site/rahuketu86.github.io" )
+
+(defvar website-html-preamble
+  "
+<nav>
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="/">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+</nav>
+")
+
+
+(defvar website-html-postamble 
+  "<div class='footer'>
+Copyright 2016 %a (%v HTML).<br>
+Last updated %C. <br>
+</div>")
 
 (setq org-publish-project-alist
       '(
         ("website" :components ("orgfiles"))
         ("orgfiles"
-         :base-directory  "C:/rahuketu/programming/static-site/rahuketu86.github.io/src" 
+         :base-directory  ,org-mode-websrc-directory
          :base-extension "org"
-         :publishing-directory  "C:/rahuketu/programming/static-site/rahuketu86.github.io"
+         :publishing-directory  ,org-mode-publishing-directory
          :exclude "*/excluded/*" 
          :recursive t
          :publishing-function org-twbs-publish-to-html
          :headline-levels 3
+	 :section-numbers nil
+	 :html-preamble ,website-html-preamble
+	 :html-postamble ,website-html-postamble
          :with-toc nil
         )
         )
